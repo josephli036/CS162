4c27e7ab8f438253a58f0739f941f3e5e01f32d8
Joseph Li <josephli036@gmail.com>
2016-09-08 18:29:56 -0700

    added buffering and 200character padding

--- projects/proj1_chat/server.py
+++ projects/proj1_chat/server.py
@@ -4,9 +4,14 @@ import select
 
 PORT = 0
 SOCKET_LIST = []
+
+# Maps channel name to list of sockets in the channel
 channels = {'home':[]}
+
+# Maps address to (name, channel)
 client_info = {}
 RECV_BUFFER = 4096
+message_buffer = {}
 
 def join_channel(message, server_socket, client_socket):
     if message[1] not in channels:
@@ -34,23 +39,37 @@ def create_channel(message, server_socket, client_socket):
 commands = {'/join': join_channel, '/list': list_channel, '/create': create_channel}
 
 def process_message(message, server_socket, client_socket):
-    if message[0] == '/':
-        command = message.split()
-        commands[command[0]](message, server_socket, client_socket)
+    client_address = client_socket.getpeername()]
+    if client_address in message_buffer:
+        message_buffer[client_address] += message
     else:
-        message.rstrip()
+        message_buffer[client_address] = message
+    if len(message_buffer[client_address]) >= 200:
+        output = message_buffer[client_address][:200]
+        if len(message_buffer[client_address]) == 200:
+            message_buffer.pop(client_address)
+        else:
+            message_buffer[client_address] = message_buffer[client_socket][200:]
+        if output[0] == '/':
+            command = output.rstrip().split()
+            commands[command[0]](output, server_socket, client_socket)
+        else:
+            channel_broadcast(output, client_socket)
+
+def channel_broadcast(message, client_socket):
+    message = message.rstrip()
         client_channel = client_info[client_socket.getpeername()][1]
         print message
         for s in SOCKET_LIST:
             if s != server_socket and s != client_socket and s in channels[client_channel]:
                 message = '[' + client_info[s.getpeername()][0] + ']' + message
+                message += '' * (200 - len(message))
                 s.send(message)
-
 
 def server():
 
     server_socket = socket.socket()
-    server_socket.bind(('127.0.0.1', PORT))
+    server_socket.bind(('127.0.0.1', PORT)) 
     server_socket.listen(10)
 
     SOCKET_LIST.append(server_socket)
