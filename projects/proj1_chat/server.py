1972b982884bddee48655ed11b33227c1f6cbb5b
Joseph Li <josephli036@gmail.com>
2016-09-08 18:47:02 -0700

    added error messaging

--- projects/proj1_chat/server.py
+++ projects/proj1_chat/server.py
@@ -1,6 +1,7 @@
 import sys
 import socket
 import select
+import utils
 
 PORT = 0
 SOCKET_LIST = []
@@ -14,32 +15,37 @@ RECV_BUFFER = 4096
 message_buffer = {}
 
 def join_channel(message, server_socket, client_socket):
-    if message[1] not in channels:
-        client_socket.send('There is no channel named %s' % message[1])
+    if len(message) < 2:
+        single_client_message(SERVER_NO_CHANNEL_EXISTS.format(message[1]), client_socket)
+    elif message[1] not in channels:
+        single_client_message(SERVER_JOIN_REQUIRES_ARGUMENT.format(message[1]), client_socket)
     else:
-        client_channel = client_info[s.getpeername()]
-        channels[client_channel[1]].remove(client_socket)
+        client = client_info[s.getpeername()]
+        channel_broadcast(SERVER_CLIENT_LEFT_CHANNEL.format(client[0]))
+        channels[client[1]].remove(client_socket)
         channels[message[1]].append(client_socket)
-        client_info[s.getpeername()] = (client_channel[0], message[1])
+        client_info[s.getpeername()] = (client[0], message[1])
+        channel_broadcast(SERVER_CLIENT_JOINED_CHANNEL.format(client[0]))
 
 def list_channel(message, server_socket, client_socket):
     for channel in channels:
         client_socket.send[channel]
 
 def create_channel(message, server_socket, client_socket):
-    if message[1] in channel:
-        client_socket.send('There is already a channel named %s' % message[1])
+    if len(message) < 2:
+        single_client_message(SERVER_CREATE_REQUIRES_ARGUMENT.format(message[1]), client_socket)
+    elif message[1] in channel:
+        single_client_message(SERVER_CHANNEL_EXISTS.format(message[1]), client_socket)
     else:
         client_channel = client_info[s.getpeername()]
         channels[client_channel[1]].remove(client_socket)
         channels[message[1]] = [client_socket]
         client_info[s.getpeername()] = (client_channel[0], message[1])
 
-
 commands = {'/join': join_channel, '/list': list_channel, '/create': create_channel}
 
 def process_message(message, server_socket, client_socket):
-    client_address = client_socket.getpeername()]
+    client_address = client_socket.getpeername()
     if client_address in message_buffer:
         message_buffer[client_address] += message
     else:
@@ -52,19 +58,26 @@ def process_message(message, server_socket, client_socket):
             message_buffer[client_address] = message_buffer[client_socket][200:]
         if output[0] == '/':
             command = output.rstrip().split()
-            commands[command[0]](output, server_socket, client_socket)
+            try:
+                commands[command[0]](output, server_socket, client_socket)
+            except:
+                single_client_message(SERVER_INVALID_CONTROL_MESSAGE.format(command[0]), client_socket)
         else:
             channel_broadcast(output, client_socket)
 
+def single_client_message(message, client_socket):
+    message += '' * (200 - len(message))
+    client_socket.send(message)
+
 def channel_broadcast(message, client_socket):
     message = message.rstrip()
-        client_channel = client_info[client_socket.getpeername()][1]
-        print message
-        for s in SOCKET_LIST:
-            if s != server_socket and s != client_socket and s in channels[client_channel]:
-                message = '[' + client_info[s.getpeername()][0] + ']' + message
-                message += '' * (200 - len(message))
-                s.send(message)
+    client_channel = client_info[client_socket.getpeername()][1]
+    print message
+    for s in SOCKET_LIST:
+        if s != server_socket and s != client_socket and s in channels[client_channel]:
+            message = '[' + client_info[s.getpeername()][0] + ']' + message
+            message += '' * (200 - len(message))
+            s.send(message)
 
 def server():
 
