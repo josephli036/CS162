6daa01a32771caed412bc593cf01cded65f972e4
Joseph Li <josephli036@gmail.com>
2016-10-10 07:52:13 -0700

    working on part 2

--- projects/proj2_routing/dv_router.py
+++ projects/proj2_routing/dv_router.py
@@ -100,7 +100,8 @@ class DVRouter(basics.DVRouterBase):
                 old_latency = self.dst_latency_lookup[root]
                 new_latency = self.dst_latency_lookup[p_from] + r_latency
                 if new_latency <= old_latency:
-                    # self.port_dst_lookup[self.dst_port_lookup[root]].remove(root)
+                    self.log("sdfjklslkjghdskj (%s)", self.port_dst_lookup[self.dst_port_lookup[root]])
+                    self.port_dst_lookup[self.dst_port_lookup[root]].remove(root)
                     self.port_dst_lookup[port] += [root]
                     self.dst_port_lookup[root] = port
                     self.dst_latency_lookup[root] = new_latency
