c629910dc26ad24346bf9b2a696e012b043235f9
Joseph Li <josephli036@gmail.com>
2016-10-10 05:13:27 -0700

    working on part 2

--- projects/proj2_routing/dv_router.py
+++ projects/proj2_routing/dv_router.py
@@ -70,7 +70,7 @@ class DVRouter(basics.DVRouterBase):
             if root not in self.dst_port_lookup:
                 changed = True
                 self.dst_port_lookup[root] = port
-                if port in port_dst_lookup:
+                if port in self.port_dst_lookup:
                     self.port_dst_lookup[port] += [root]
                 else:
                     self.port_dst_lookup[port] = [root]
