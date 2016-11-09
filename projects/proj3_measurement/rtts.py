9d7dbda6d96b3e4a55f4ad4eb71490cf5ce563b7
Joseph Li <josephli036@gmail.com>
2016-11-09 12:45:58 -0800

    fdksgjlj

--- projects/proj3_measurement/rtts.py
+++ projects/proj3_measurement/rtts.py
@@ -19,19 +19,25 @@ def run_ping(hostnames, num_packets, raw_ping_output_filename, aggregated_ping_o
         ping = subprocess.Popen(["ping", "-n", num_packets, host], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, error = ping.communicate()
         if out:
-            maximum = float(re.findall(r"Maximum = (\d+)", out)[0])
-            lost = int(re.findall(r"Lost = (\d+)", out)[0])
+            lost = int(re.findall(r".*(\d+)% packet loss", out)[0])
             split = out.split('\n')
             rtts = []
+            seq = 0
             for i in split:
                 # print i
                 rtt = re.findall(r".*time=(\d+)", i)
-                if rtt:
-                    # print float(rtt[0])
-                    rtts.append(float(rtt[0]))
-                elif i == "Request timed out.\r":
-                    # print -1.0
-                    rtts.append(-1.0)
+                new_seq = int(re.findall(r".*icmp_seq=(\d+)", i)[0])
+                if new_seq:
+                    if new_seq-seq == 1:
+                        # print float(rtt[0])
+                        rtts.append(float(rtt[0]))
+                        seq++
+                    else:
+                        while new_seq-seq != 1:
+                            rtts.append(-1.0)
+                            seq++
+                        rtts.append(float(rtt[0]))
+                        seq++
             raw_output[host] = rtts
             agg = {}
             agg["drop_rate"] = 100 * float(lost)/float(num_packets)
@@ -41,7 +47,7 @@ def run_ping(hostnames, num_packets, raw_ping_output_filename, aggregated_ping_o
                 agg["median_rtt"] = -1.0
             else:
                 agg["drop_rate"] = 100 * float(lost)/float(num_packets)
-                agg["max_rtt"] = maximum
+                agg["max_rtt"] = sorted(rtts)[len(rtts)]
                 agg["median_rtt"] = median(rtts[lost:])
             agg_output[host] = agg
         else:
