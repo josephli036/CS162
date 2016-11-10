ba79391aa1fdfe92caab5dda3cddedfc856ac1ac
Joseph Li <josephli036@gmail.com>
2016-11-09 20:54:24 -0800

    fdksgjlj

--- projects/proj3_measurement/traceroute.py
+++ projects/proj3_measurement/traceroute.py
@@ -15,19 +15,31 @@ def run_traceroute(hostnames, num_packets, output_filename):
 
 def parse_traceroute(raw_traceroute_filename, output_filename):
     file = open(raw_traceroute_filename)
+    result = {}
     whole_output = file.read()
     whole_output = whole_output.split("traceroute")[1:]
     for trace in whole_output:
         trace = trace.split('\n')
+        hostname = re.findall(r"(\S*) \(", trace)[0]
+        result[hostname] = []
         for line in trace[1:len(trace)-1]:
             print line
             name = re.findall(r"\d  (\S*)", line)[0]
             if name == "*":
-                name = "None"
-                ip = "None"
+                result[hostname].append([{"name": "None", "ip": "None", "ASN": "None"}])
             else:
+                name = re.findall(r"(\S*) \(", line)
                 ip = re.findall(r"\((.*)\)", line)
-                print ip
+                asn = re.findall(r"\[(.*)\]", line)
+                total = []
+                for i in range(0, len(name)-1):
+                    temp = {"name" = name[i], "ip" = ip[i]}
+                    if asn[i] == "*":
+                        temp["ASN"] = "None"
+                    else:
+                        temp["ASN"] = asn[i]
+
+                
 
 file = open('alexa_top_100')
 websites = []
