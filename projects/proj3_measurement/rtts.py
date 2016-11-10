08be67183d832b9ef3e4d7d22628251b2bbf063a
Joseph Li <josephli036@gmail.com>
2016-11-09 16:50:10 -0800

    fdksgjlj

--- projects/proj3_measurement/rtts.py
+++ projects/proj3_measurement/rtts.py
@@ -41,7 +41,7 @@ def run_ping(hostnames, num_packets, raw_ping_output_filename, aggregated_ping_o
             raw_output[host] = rtts
             agg = {}
             if lost == 100:
-                while len(rtts) != num_packets:
+                while len(rtts) != int(num_packets):
                     rtts.append(-1.0)
                 agg["drop_rate"] = 100.0
                 agg["max_rtt"] = -1.0
@@ -63,4 +63,4 @@ file = open('alexa_top_100')
 websites = []
 for website in file.readlines():
     websites.append(website.rstrip())
-run_ping(websites, '1', 'rtt_a_raw.json', 'rtt_a_agg.json')
+run_ping(websites, '5', 'rtt_a_raw.json', 'rtt_a_agg.json')
