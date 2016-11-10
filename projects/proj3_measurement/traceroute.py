f7a7212de143496375408db1a3e7df1fa73df27e
Joseph Li <josephli036@gmail.com>
2016-11-09 19:18:06 -0800

    fdksgjlj

---
+++ projects/proj3_measurement/traceroute.py
@@ -0,0 +1,21 @@
+import subprocess
+import re
+import json
+
+def run_traceroute(hostnames, num_packets, output_filename):
+    for host in hostnames:
+        ping = subprocess.Popen(["ping", "-a", "-q", num_packets, host], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, error = ping.communicate()
+        if out:
+            file = open(output_filename)
+            file.write(out)
+        else:
+            print 'No trace'
+
+file = open('alexa_top_100')
+websites = []
+for website in file.readlines():
+    websites.append(website.rstrip())
+run_traceroute(websites, "1", 'traceoutput')
+
+#["google.com", "facebook.com", "www.berkeley.edu", "allspice.lcs.mit.edu", "todayhumor.co.kr", "www.city.kobe.lg.jp", "www.vutbr.cz", "zanvarsity.ac.tz"]
