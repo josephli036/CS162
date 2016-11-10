4f6bb478e67eea7cf08aff6eab0b9a15dc2ec9a9
Joseph Li <josephli036@gmail.com>
2016-11-09 20:08:36 -0800

    fdksgjlj

--- projects/proj3_measurement/traceroute.py
+++ projects/proj3_measurement/traceroute.py
@@ -13,10 +13,19 @@ def run_traceroute(hostnames, num_packets, output_filename):
         else:
             print 'No trace'
 
+def parse_traceroute(raw_traceroute_filename, output_filename):
+    file = open(raw_traceroute_filename)
+    whole_output = file.read()
+    whole_output = whole_output.split("traceroute")[1:]
+    for trace in whole_output:
+        print [trace]
+
+
+
 file = open('alexa_top_100')
 websites = []
+trace_a_websites = ["google.com", "facebook.com", "www.berkeley.edu", "allspice.lcs.mit.edu", "todayhumor.co.kr", "www.city.kobe.lg.jp", "www.vutbr.cz", "zanvarsity.ac.tz"]
 for website in file.readlines():
     websites.append(website.rstrip())
-run_traceroute(websites, "1", 'traceoutput')
-
-#["google.com", "facebook.com", "www.berkeley.edu", "allspice.lcs.mit.edu", "todayhumor.co.kr", "www.city.kobe.lg.jp", "www.vutbr.cz", "zanvarsity.ac.tz"]
+run_traceroute(trace_a_websites, "5", 'traceoutput')
+parse_traceroute("traceoutput", "tr_a.json")
