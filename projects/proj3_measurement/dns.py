67003e9c7af956d8d6aede9d35a3e332a6b9ffad
Joseph Li <josephli036@gmail.com>
2016-11-10 19:00:52 -0800

    fdksgjlj

--- projects/proj3_measurement/dns.py
+++ projects/proj3_measurement/dns.py
@@ -30,11 +30,13 @@ def parse_no_dns(out, host_dictionary):
         lines = query.split('\n')
         if lines[1][0] == ';':
             lines = lines[3:]
+        print lines
         result = lines[len(lines)-1]
         lines = lines[:len(lines)-1]
         for line in lines:
             queried_name = re.findall(r"^(\S+)\t\t\t", line)
             TTL = re.findall(r"^.*\.\t\t\t(\d+)\t", line)
+            print line
             print queried_name
             print TTL
 
