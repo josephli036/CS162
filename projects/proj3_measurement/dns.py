a538c4dd80a21d58b05bb3647f8f1d33cdc2c0ca
Joseph Li <josephli036@gmail.com>
2016-11-10 17:39:28 -0800

    fdksgjlj

--- projects/proj3_measurement/dns.py
+++ projects/proj3_measurement/dns.py
@@ -3,25 +3,25 @@ import re
 import json
 
 def run_dig(hostname_filename, output_filename, dns_query_server=None):
-	websites = []
-	result = []
-	with open('alexa_top_100') as file:
-		for website in file.readlines():
-	    	websites.append(website.rstrip())
+    websites = []
+    result = []
+    with open('alexa_top_100') as file:
+        for website in file.readlines():
+            websites.append(website.rstrip())
 
     websites = ["google.com"]
 
     for host in websites:
-    	host_dictionary = {"Name": }
-	    if dns_query_server:
-	    	dns_run = subprocess.Popen(["dig", host, "@"+str(dns_query_server)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	    else:
-			dns_run = subprocess.Popen(["dig", "+trace", "+tries=1", "+nofail", host], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	    out, error = dns_run.communicate()
+        host_dictionary = {"Name": }
+        if dns_query_server:
+            dns_run = subprocess.Popen(["dig", host, "@"+str(dns_query_server)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        else:
+            dns_run = subprocess.Popen(["dig", "+trace", "+tries=1", "+nofail", host], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, error = dns_run.communicate()
 
-	    if out:
-	    	out.split('\n')
-	    	print out
+        if out:
+            out.split('\n')
+            print out
 
 
 run_dig("sdfsdfsdf", "asdfsadf")
