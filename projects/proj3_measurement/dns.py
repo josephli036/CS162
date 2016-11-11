148b2c3c9c67542902b4b76d1696f8979efcc969
Joseph Li <josephli036@gmail.com>
2016-11-10 18:39:22 -0800

    fdksgjlj

--- projects/proj3_measurement/dns.py
+++ projects/proj3_measurement/dns.py
@@ -16,18 +16,27 @@ def run_dig(hostname_filename, output_filename, dns_query_server=None):
         if dns_query_server:
             dns_run = subprocess.Popen(["dig", host, "@"+str(dns_query_server)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         else:
-            dns_run = subprocess.Popen(["dig", "+trace", "+tries=1", "+nofail", host], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, error = dns_run.communicate()
+            dns_run = subprocess.Popen(["dig", "+trace", "+tries=1", "+nofail", "+nodnssec", host], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, error = dns_run.communicate()
+            if out:
+                host_dictionary = parse_no_dns(out, host_dictionary)
 
-        if out:
-            out = out.split('\n\n')
-            out = out[:len(out)-1]
-            for query in out:
-                lines = query.split('\n')
-                if lines[1][0] == ';':
-                    lines = lines[2:]
-                for line in lines:
-                    print [lines]
+def parse_no_dns(out, host_dictionary):
+    out = out.split('\n\n')
+    out = out[:len(out)-1]
+
+    for query in out:
+        success = False
+        lines = query.split('\n')
+        if lines[1][0] == ';':
+            lines = lines[3:]
+        for line in lines:
+            print line
+
+            
+    return None
+
+
 
 
 run_dig("sdfsdfsdf", "asdfsadf")
